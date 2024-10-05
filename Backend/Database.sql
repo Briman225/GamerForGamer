b4f6293b4f471aeac3aaa5390c3497cf5fe8a3e1
Briman225 <briman225@gmail.com>
2024-10-05 02:04:37 -0400

    Added Database Query and JSON

---
+++ Backend/Database.sql
@@ -0,0 +1,43 @@
+DROP DATABASE IF EXISTS `G4G`;
+CREATE DATABASE `G4G`;
+USE `G4G`;
+
+CREATE TABLE user_profiles (
+	user_id INT NOT NULL,
+    username VARCHAR(50),
+    pass VARCHAR(50),
+    first_name VARCHAR(50),
+    last_name VARCHAR(50),
+    age INT,
+    zip_code INT NOT NULL,
+    PRIMARY KEY (user_id)
+);
+
+CREATE TABLE games (
+	game_id INT NOT NULL,
+    game_title VARCHAR(200),
+    PRIMARY KEY (game_id)
+);
+
+CREATE TABLE conversations (
+  conversation_id INT NOT NULL,
+  participants JSON,
+  messages JSON,
+  conversation_meta JSON,
+  PRIMARY KEY (conversation_id)
+);
+
+CREATE TABLE matches (
+	user_id_a INT NOT NULL,
+    user_id_b INT NOT NULL,
+    FOREIGN KEY (user_id_a) REFERENCES users(user_id),
+    FOREIGN KEY (user_id_b) REFERENCES users(user_id)
+);
+
+CREATE TABLE selections (
+	user_id INT NOT NULL,
+    game_id INT NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES user_profiles(user_id),
+    FOREIGN KEY (game_id) REFERENCES games(game_id)
+);
+
